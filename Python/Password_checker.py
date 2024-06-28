8a11875d68fd85e48d889cbb581b9e881d5024e4
Mokshada Vyas <144912196+MokshadaVyas@users.noreply.github.com>
2024-06-28 18:18:18 +0530

    Add files via upload

---
+++ Python/Password_checker.py
@@ -0,0 +1,7 @@
+username=input("Enter your name: ")
+password=input("Enter your password: ")
+
+password_length=len(password)
+hidden_password= "*" * password_length
+
+print(f"{username},your password {hidden_password} is {password_length} letters long")
